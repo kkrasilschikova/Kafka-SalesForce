cdc31e3297ce488ed945723f8cc1913132627b07
kkrasilschikova <Zenya22fatkina>
2017-08-03 18:48:51 +0300

    kafka IP and port are now parameters

--- src/main/scala/KafkaSalesForce/EntryPoint.scala
+++ src/main/scala/KafkaSalesForce/EntryPoint.scala
@@ -1,10 +1,12 @@
 package KafkaSalesForce
 
 object EntryPoint {
-  case class Config(sourceTopic: String="", targetTopic: String="")
+  case class Config(kafkaIpPort: String="", sourceTopic: String="", targetTopic: String="")
 
   def main(args: Array[String]): Unit = {
     val parser=new scopt.OptionParser[Config]("KafkaSalesForce") {
+      opt[String]("kafkaIP:port").required().valueName("<kafka-IP:9092").
+        action((x,c) => c.copy(kafkaIpPort=x))
       opt[String]("sourceTopic").required().valueName("<source-kafka-topic>").
         action((x,c) => c.copy(sourceTopic=x))
       opt[String]("targetTopic").required().valueName("<target-kafka-topic>").
@@ -13,19 +15,17 @@ object EntryPoint {
     parser.parse(args, Config()) match {
       case Some(config) =>
 
-        //args(0)="SF_query_requests"
-        //args(1)="SF_query_results"
-
-        val kafka = new KafkaConsumerAndProducer
-        val cases: List[String] = kafka.consumerResults(args(0))
+        val kafka = new KafkaConsumerAndProducer(config.kafkaIpPort)
+        val cases: List[String] = kafka.consumerResults(config.sourceTopic)
 
         val sf = new SalesForceConnectAndQuery
         sf.connect
         val sfResults: List[String] = sf.accountNameFromSF(cases)
 
-        for (value <- sfResults) kafka.producerUpdate(args(1), "key", value)
-
+        for (value <- sfResults) kafka.producerUpdate(config.targetTopic, "AccountLookup", value)
+        //kafka.consumerResults(config.targetTopic)//just to check target topic
       case None => // arguments are bad, error message will be displayed
     }
   }
+
 }
